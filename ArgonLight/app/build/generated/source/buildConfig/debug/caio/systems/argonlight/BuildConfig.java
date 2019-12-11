7eafaca32c02e12c0f37c0f08b97aca18e049cc3
Caio Vieira <caio.vc91@gmail.com>
2019-12-11 19:04:04 -0300

    Enviado atualização

---
+++ ArgonLight/app/build/generated/source/buildConfig/debug/caio/systems/argonlight/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package caio.systems.argonlight;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "caio.systems.argonlight";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
