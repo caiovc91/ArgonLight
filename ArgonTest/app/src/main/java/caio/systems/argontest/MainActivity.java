094db0485b2e20e6acd1917771dab8f756bf1eb2
Caio Vieira <caio.vc91@gmail.com>
2020-01-09 14:24:09 -0300

    adicionado links de consulta

--- ArgonTest/app/src/main/java/caio/systems/argontest/MainActivity.java
+++ ArgonTest/app/src/main/java/caio/systems/argontest/MainActivity.java
@@ -3,11 +3,15 @@ package caio.systems.argontest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 /*
  * Ver mais em:
+ * NavHostFragment
+ * https://code.tutsplus.com/articles/how-to-use-bottom-sheets-with-the-design-support-library--cms-26031
  * https://medium.com/@droidbyme/android-bottom-sheet-7e9cfcec6427
  * https://medium.com/android-bits/android-bottom-sheet-30284293f066
  * https://www.androidhive.info/2017/12/android-working-with-bottom-sheet/
@@ -24,5 +28,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ativ = new Activity();
+    }
+
+    public void openMenu(View view) {
+        Intent i = new Intent(this, MenuActivity.class);
+        this.startActivity(i);
     }
 }
